65138997e56c7a2aafaf858af9408394604bf095
jatin251212 <159692753+jatin251212@users.noreply.github.com>
2024-02-22 16:06:19 +0530

    Fixing size of News Components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -14,6 +14,7 @@ const NewsItem =(props)=> {
             }
             className="card-img-top"
             alt="..."
+            style={{height:'300px'}}
           />
           <div className="card-body ">
             <h5 className="card-title">
